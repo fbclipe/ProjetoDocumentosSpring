feaf9780e2b7496c7af6a560eb73d9f45f609e7b
fbclipe <fbc@cesar.school>
2025-03-27 16:39:41 -0300

    imagens estao saindo com o conteudo

--- challenge/src/main/java/com/extraidados/challenge/service/FileSaveService.java
+++ challenge/src/main/java/com/extraidados/challenge/service/FileSaveService.java
@@ -2,16 +2,17 @@ package com.extraidados.challenge.service;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.extraidados.challenge.entity.Documents;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class FileSaveService {
-    final ObjectMapper objectMapper = new ObjectMapper();
+    //final ObjectMapper objectMapper = new ObjectMapper();
     final String filePath = "C:\\Users\\carva\\OneDrive\\Área de Trabalho\\DESAFIO EXTRAIDADOS\\challenge\\challenge\\arquivos\\";
 
     public void saveDocuments(Documents documents, MultipartFile file) {
@@ -20,7 +21,10 @@ public class FileSaveService {
             if (!folders.exists()) {
                 folders.mkdirs();
             }
-            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(filePath + file.getOriginalFilename()), documents);
+            Path destino = Path.of(filePath, file.getOriginalFilename());
+
+            //objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(filePath + file.getOriginalFilename()), documents);
+            Files.write(destino, file.getBytes(), StandardOpenOption.CREATE);
         } catch(IOException e){
             e.printStackTrace();
         }    
